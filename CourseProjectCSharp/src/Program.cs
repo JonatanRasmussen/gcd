7ae486ecd9504ab40f6c01baad4881f03713fd62
JonatanRasmussen <115923496+JonatanRasmussen@users.noreply.github.com>
2023-09-29 18:49:11 +0200

    test commit

--- CourseProjectCSharp/src/Program.cs
+++ CourseProjectCSharp/src/Program.cs
@@ -16,34 +16,9 @@ class Program
         return a + b;
     }
 
-    PersonClass person = new PersonClass("John", "Doe", 30);
-    // This is a single-line comment
-
-    /*
-        This is a multi-line comment.
-        You can add more lines here.
-    */
-
     static void Main(string[] args)
     {
-        // Reading the JSON file into a string
-        string fileName = "Type of assessment";
-        string jsonText = File.ReadAllText($"infoDictTest/{fileName}.json");
-
-        // Deserializing the JSON string into a Dictionary<string, int>
-        Dictionary<string, int> myDict = JsonSerializer.Deserialize<Dictionary<string, int>>(jsonText);
-
-        // Create a list of key-value pairs from the dictionary
-        var list = myDict.ToList();
-
-        // Sort the list by value
-        list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
-
-        // Print out the sorted entries
-        foreach (var pair in list)
-        {
-            Console.WriteLine($"{pair.Key} => {pair.Value}");
-        }
+        Console.WriteLine($"{AddNumbers(1, 2)}");
     }
 }
 
